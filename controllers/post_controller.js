93e3f90681877eed9b10ddc7b332bdbc9a57a0d5
Nick Damario <nicholasgd@comcast.net>
2020-12-01 13:47:40 -0500

    all html routes hit

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -8,45 +8,62 @@ var router = express.Router();
 var post = require("../models/post.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
-//   post.Post.All(function(data) {
-//     var hbsObject = {
-//       post: data
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
-//   });
-res.render("index")
+router.get("/", function (req, res) {
+    //   post.Post.All(function(data) {
+    //     var hbsObject = {
+    //       post: data
+    //     };
+    //     console.log(hbsObject);
+    //     res.render("index", hbsObject);
+    //   });
+    res.render("index")
 });
 
-router.post("/api/post", function(req, res) {
-  post.Post.create(["post_name", "post_content", "post_tags", "user_id"], [req.body.post_name, req.body.post_content, req.body.post_tags, req.body.user.id], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
+router.get("/signup", function (req, res) {
+
+    res.render("signup")
 });
 
-router.put("/api/post/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
+router.get("/login", function (req, res) {
 
-  console.log("condition", condition);
+    res.render("login")
+});
 
-  post.Post.update(
-    {
-      post_name: req.body.post_name,
-      post_content: req.body.post_content,
-      post_tags: req.body.post_tags,
-    },
-    condition,
-    function(result) {
-      if (result.changedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+router.get("/profile", function (req, res) {
 
-    }
-  );
+    res.render("profile")
+});
+
+
+router.post("/api/post", function (req, res) {
+    post.Post.create(["post_name", "post_content", "post_tags", "user_id"], [req.body.post_name, req.body.post_content, req.body.post_tags, req.body.user.id], function (result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+        res.render("userPosts")
+    });
+});
+
+router.put("/api/post/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    post.Post.update(
+        {
+            post_name: req.body.post_name,
+            post_content: req.body.post_content,
+            post_tags: req.body.post_tags,
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                // If no rows were changed, then the ID must not exist, so 404
+                return res.status(404).end();
+            }
+            res.status(200).end();
+
+        }
+    );
 });
 
 // Export routes for server.js to use.
