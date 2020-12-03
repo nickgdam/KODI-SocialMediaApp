b9b99a4c6286fe9b67021afb781842b220af106c
Nick Damario <nicholasgd@comcast.net>
2020-12-02 21:00:01 -0500

    updating post user function

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,6 +52,20 @@ module.exports = function (app) {
         });
     });
 
+    app.post("/api/signup", function (req, res) {
+        console.log(req.body);
+        // create takes an argument of an object describing the item we want to
+        // insert into our table. In this case we just we pass in an object with a text
+        // and complete property (req.body)
+        db.User.create({
+            user_name: req.body.username,
+            password: req.body.password
+        }).then(function (social_db) {
+            // We have access to the new todo as an argument inside of the callback function
+            res.json(social_db);
+        });
+    });
+
     // DELETE route for deleting todos. We can get the id of the todo we want to delete from
     // req.params.id
     app.delete("/api/todos/:id", function (req, res) {
