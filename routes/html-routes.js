8ccb8e5624fe177ae6d8eaaa1fb6c5396c3621e5
Nick Damario <nicholasgd@comcast.net>
2020-12-04 17:47:25 -0500

    updating models and routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,6 +17,11 @@ module.exports = function (app) {
     res.render("login")
   });
 
+  // app.get("/profile", (req, res) => {
+  //   // If the user already has an account send them to the members page
+  //   res.render("profile")
+  // });
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/signup", (req, res) => {
@@ -36,6 +41,23 @@ module.exports = function (app) {
 
 
   });
+  
+  app.get("/userposts/:id", function (req, res) {
+    // findAll returns all entries for a table when used with no options
+   db.Posts.findAll({
+       where:{
+           id:req.params.id
+       }
+   }).then((dbPost) => {
+        // We have access to the todos as an argument inside of the callback function
+        res.render("userposts", { Posts: dbPost })
+    }) 
+        .catch(function (err) {
+            console.log(err);
+        });
+  });
+
+
   app.get("/allUsers", (req, res) => {
     db.User.findAll({}).then((dbUser) => {
       // We have access to the todos as an argument inside of the callback function
@@ -46,22 +68,31 @@ module.exports = function (app) {
       });
   });
 
-  app.get("/profile",
-    // passport.authenticate("local"),
+  app.get("/profile/:id",
+    passport.authenticate("local"),
     (req, res) => {
-      if (!req.user) {
-        // The user is not logged in, send back an empty object
-        res.json({});
-      } else {
-        // Otherwise send back the user's email and id
-        // Sending back a password, even a hashed password, isn't a good idea
+      db.User.findAll({
+        where: {
+          id: req.params.id
+        }
+      }).then(() => {
+        // We have access to the todos as an argument inside of the callback function
         res.render("profile", {
           user_name: req.user.user_name,
           firstName: req.user.firstName,
           lastName: req.user.lastName,
           biography: req.user.biography
+        })
+        .catch(err => {
+          res.status(401).json(err)
+          console.log(err)
         });
-      }
+    })
+      
+        // Otherwise send back the user's email and id
+        // Sending back a password, even a hashed password, isn't a good idea
+       
+      
 
 
 
@@ -76,7 +107,9 @@ module.exports = function (app) {
   
 
   
-app.get("/userposts/:id", function (req, res) {
+
+
+app.delete("/userposts/:id", function (req, res) {
   // findAll returns all entries for a table when used with no options
  db.Posts.findAll({
      where:{
